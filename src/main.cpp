d2077e5fa60e1fb0f0338f2dd23ffea69a224c3f
Wiktor Kwiatkowski <wiktorkwiatkowski143@gmail.com>
2025-05-21 20:39:20 +0200

    computing rpm and low pass filtering works

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Adafruit_INA219.h>
 #include <Arduino.h>
+
+#include "low_pass.h"
 // Piny dla enkodera
 #define ENCODER_AY 19  // Kanał A enkodera (żółty) - przerwanie na zbocze narastające CW
 #define ENCODER_BG 23  // Kanał B enkodera (zielony) - odczytywany w ISR CCW
@@ -14,23 +16,24 @@
 
 #define START_BYTE 0xA5          // Początek ramki
 #define START_BYTE_RECEIVE 0xB5  // Początek ramki odbieranej
-#define FRAME_LENGTH 35          // Długość danych w ramce
+#define FRAME_LENGTH 32          // Długość danych w ramce
 
 // Długość ramki odbiorczej
 #define RX_FRAME_LENGTH 7
 
 enum DataType : uint8_t {
     TYPE_PWM = 0x01,
-    TYPE_KP = 0x02,
-    TYPE_KI = 0x03,
-    TYPE_KD = 0x04
-    // Dodaj więcej w razie potrzeby
+    TYPE_RPM = 0x02,
+    TYPE_KP = 0x03,
+    TYPE_KI = 0x04,
+    TYPE_KD = 0x05,
+    TYPE_MODE = 0x06,
+    TYPE_START_STOP = 0x07
 };
+enum Mode : uint8_t { Manual = 0x00, Automatic = 0x01 };
 
-enum Mode : uint8_t {
-    Manual = 0x00,
-    Automatic = 0x01
-};
+Mode mode = Mode::Manual;
+bool motor_enabled = false;
 
 // Bufor odbiorczy
 uint8_t rx_buffer[RX_FRAME_LENGTH];
@@ -59,12 +62,17 @@ int pos_prev = 0;  // Poprzednia pozycja enkodera
 volatile long pos_i = 0;
 
 // Filtrowanie dolnoprzepustowe
-float v1_filt = 0;
-float v1_prev = 0;
-float v2_filt = 0;
-float v2_prev = 0;
-int pwr = 0;
+LowPass<2> filter_rpm(5.0f, 100.0f, true);  // filtr 2. rzędu, 5 Hz, 100 Hz próbkowanie
+LowPass<2> filter_current(5.0f, 100.0f, true);
+LowPass<2> filter_voltage(5.0f, 100.0f, true);
+LowPass<2> filter_power(5.0f, 100.0f, true);
 
+float v1_filt = 0;
+
+uint8_t pwr = 0;
+uint8_t pwr_before_stop = 0;
+
+// Parametry PID
 float kp = 0;
 float ki = 0;
 float kd = 0;
@@ -72,14 +80,12 @@ float kd = 0;
 // PID
 float eintegral = 0;
 float eprev = 0;
-
-unsigned long lastPwrUpdate = 0;
-bool pwrIncreasing = true;
+float vt = 0;
 
 // Deklaracje funkcji
 void read_encoder();
 void IRAM_ATTR set_motor(Direction, int, int, int, int);
-void send_qt_binary(float, int, float, float, float, float, float, float, Mode);
+void send_qt_binary(float, uint8_t, float, float, float, float, float, float, Mode);
 void debug_data(uint8_t *, size_t length);
 // Funkcja obliczająca checksum (XOR wszystkich bajtów)
 uint8_t calculate_checksum(uint8_t *, int);
@@ -90,17 +96,18 @@ void simulate_qt_frame_debug();
 void setup() {
     Serial.begin(115200);
     Wire.begin();
-    // while (!ina219.begin()) {
-    //     Serial.println("Failed to find INA219 chip");
-    //     delay(100);
-    // }
+    while (!ina219.begin()) {
+        Serial.println("Failed to find INA219 chip");
+        digitalWrite(2, HIGH);
+        delay(100);
+    }
     pinMode(ENCODER_AY, INPUT);
     pinMode(ENCODER_BG, INPUT);
     pinMode(IN1, OUTPUT);
     pinMode(IN2, OUTPUT);
     attachInterrupt(digitalPinToInterrupt(ENCODER_AY), read_encoder, RISING);
     pinMode(2, OUTPUT);
-    // ina219.setCalibration_32V_1A();
+    ina219.setCalibration_16V_400mA();
     delay(500);
 }
 
@@ -117,93 +124,62 @@ void loop() {
     // Obliczenie czasu deltaT
     long curr_t = micros();
     float delta_t = ((float)(curr_t - prev_t)) / 1.0e6;
-
-    // Obliczenie prędkości (imp/s)
     float velocity1 = (pos - pos_prev) / delta_t;
     pos_prev = pos;
     prev_t = curr_t;
 
+    // Obliczenie prędkości (imp/s)
+    float v1 = velocity1 / (GEAR_RATIO * ENCODER_PULSES_PER_REV) * 60;
+
     // Konwersja prędkości do RPM
-    float v1 = velocity1 / GEAR_RATIO * ENCODER_PULSES_PER_REV * 60;
 
     // Filtrowanie dolnoprzepustowe (5 Hz)
-    v1_filt =
-        1.5610 * v1_filt - 0.6414 * v2_filt + 0.0201 * v1 + 0.0402 * v1_prev + 0.0201 * v2_prev;
-    v2_filt = v1_filt;
-    v2_prev = v1_prev;
-    v1_prev = v1;
-    // Wartość zadana prędkości (150 RPM sinusoidalnie zmieniana) taki przebieg prostokątny
-    float vt = 150 * (sin(curr_t / 1e6) > 0);
-    // Serial.print(">v_target:");
-    // Serial.println(vt);
-    // Parametry PID
-    kp = 1;
-    ki = 1;
-    kd = 0.1;
+    v1_filt = filter_rpm.filt(v1);
 
     // Obliczenia regulatora PID
-    float e = vt - v1_filt;
-    eintegral = eintegral + e * delta_t;
-    float dedt = (e - eprev) / delta_t;
-    float u = kp * e + ki * eintegral + kd * dedt;
-    eprev = e;
+    if (mode == Mode::Automatic) {
+        float e = vt - v1_filt;
+        eintegral = eintegral + e * delta_t;
+        float dedt = (e - eprev) / delta_t;
+        float u = kp * e + ki * eintegral + kd * dedt;
+        eprev = e;
+        pwr = (uint8_t)fminf(fabs(u), 255.0f);
+    }
 
-    // Kierunek obrotu
     dir = LEFT;
-    if (u < 0) {
-        dir = RIGHT;
+
+    if (!motor_enabled) {
+        pwr = 0;
+        set_motor(dir, ENA, pwr, IN1, IN2);  // Zatrzymaj silnik
+        digitalWrite(2, LOW);
     }
 
-    // Wartość PWM (ograniczona do 255)
-    // pwr = (int)fabs(u);
+    if (pwr > 255) pwr = 255;
 
-    // if (millis() - lastPwrUpdate >= 1000) {
-    //     lastPwrUpdate = millis();
-    //     pwr = random(0, 256);
-    // }
-
-    if (pwr > 255) {
-        pwr = 255;
-    }
-
-    // Debug prędkości
-    // Serial.println(pwr);
     set_motor(dir, ENA, pwr, IN1, IN2);
 
-    // Serial.print(">Vel1:");
-    // Serial.println(v1_filt);
+    digitalWrite(2, pwr == 255 ? HIGH : LOW);
 
     // Read voltage and current from INA219.
-    // Serial.print(">EN_Yellow:");
-    // Serial.println(5 * a);
-    // Serial.print(">EM_green:");
-    // Serial.println(5 * b);
 
-    // shunt_voltage = ina219.getShuntVoltage_mV();
-    // bus_voltage = ina219.getBusVoltage_V();
-    // current_mA = ina219.getCurrent_mA();
-    // power_mW = ina219.getPower_mW();
-    float loadvoltage = bus_voltage + (shunt_voltage / 1000);
+    shunt_voltage = ina219.getShuntVoltage_mV();
+    bus_voltage = ina219.getBusVoltage_V();
+    current_mA = ina219.getCurrent_mA();
+    power_mW = ina219.getPower_mW();
+    float load_voltage = bus_voltage + (shunt_voltage / 1000);
 
-    // Serial.print(">Bus voltage:");
-    // Serial.println(bus_voltage);
-    // Serial.println(" V");
-    // Serial.print("Shunt voltage:       ");
-    // Serial.print(shuntvoltage);
-    // Serial.println(" mV");
-    // Serial.print(">Load voltage:");
-    // Serial.println(loadvoltage);
-    // Serial.println(" V");
-    // Serial.print(">Current:");
-    // Serial.println(current_mA);
-    // Serial.println(" mA");
-    // Serial.print(">Power:");
-    // Serial.println(power_mW);
-    // Serial.println(" mW");
-    // Serial.println("");
+    // Low pass
+    current_mA = filter_current.filt(current_mA);
+    load_voltage = filter_voltage.filt(load_voltage);
+    power_mW = filter_power.filt(power_mW);
+
+    current_mA = (current_mA > 0) ? current_mA : 0;
+    load_voltage = (load_voltage > 0) ? load_voltage : 0;
+    power_mW = (power_mW > 0) ? power_mW : 0;
+    v1_filt = (v1_filt > 0) ? v1_filt : 0;
 
     // Transmisja danych do Qt
-    send_qt_binary(v1_filt, pwr, current_mA, load_voltage, power_mW, kp, ki, kd, Mode::Automatic);
+    send_qt_binary(v1_filt, pwr, current_mA, load_voltage, power_mW, kp, ki, kd, mode);
 }
 
 /*Czytamy enkoder b jak a będzie miało interupta na rising, jak kręcimy w
@@ -237,33 +213,29 @@ void set_motor(Direction dir, int pwm, int pwm_val, int in1, int in2) {
     }
 }
 
-void send_qt_binary(float rpm, int pwm, float current, float voltage, float power, float k_p, float k_i, float k_d, Mode mode) {
-    
+void send_qt_binary(float rpm, uint8_t pwm, float current, float voltage, float power, float k_p,
+                    float k_i, float k_d, Mode mode) {
     uint8_t start_byte = START_BYTE;
-    float f_pwm = static_cast<float>(pwm);
-    
-    // 1 + 1 + 5*4 23
     // Zbiór bajtów do wysłania
-    uint8_t buffer[FRAME_LENGTH];  // 35 bajty (start byte + dane + tryb + checksum)
+    uint8_t buffer[FRAME_LENGTH];  // 32 bajty (start byte + dane + tryb + checksum)
 
     // Kopiowanie danych do bufora
-    memcpy(buffer, &start_byte, sizeof(start_byte));   // Start byte
-    memcpy(buffer + 1, &rpm, sizeof(rpm));           // RPM
-    memcpy(buffer + 5, &f_pwm, sizeof(f_pwm));           // PWM
-    memcpy(buffer + 9, &current, sizeof(current));  // Current
-    memcpy(buffer + 13, &voltage, sizeof(voltage));  // Voltage
-    memcpy(buffer + 17, &power, sizeof(power));      // Power
-    memcpy(buffer + 21, &k_p, sizeof(k_p));
-    memcpy(buffer + 25, &k_i, sizeof(k_i));
-    memcpy(buffer + 29, &k_d, sizeof(k_d));
-    memcpy(buffer + 33, &mode, sizeof(mode));
-    
-    
+    memcpy(buffer, &start_byte, sizeof(start_byte));  // Start byte
+    memcpy(buffer + 1, &rpm, sizeof(rpm));            // RPM
+    memcpy(buffer + 5, &pwm, sizeof(pwm));            // PWM
+    memcpy(buffer + 6, &current, sizeof(current));    // Current
+    memcpy(buffer + 10, &voltage, sizeof(voltage));   // Voltage
+    memcpy(buffer + 14, &power, sizeof(power));       // Power
+    memcpy(buffer + 18, &k_p, sizeof(k_p));
+    memcpy(buffer + 22, &k_i, sizeof(k_i));
+    memcpy(buffer + 26, &k_d, sizeof(k_d));
+    memcpy(buffer + 30, &mode, sizeof(mode));
+
     // Oblicz checksum
-    uint8_t checksum = calculate_checksum(buffer, sizeof(buffer) - 1);  
+    uint8_t checksum = calculate_checksum(buffer, sizeof(buffer) - 1);
 
     // Dodanie checksum do ramki
-    memcpy(buffer + 34, &checksum, sizeof(checksum));
+    memcpy(buffer + 31, &checksum, sizeof(checksum));
 
     // Wysyłanie ramki
     Serial.write(buffer, sizeof(buffer));
@@ -315,20 +287,41 @@ void parse_qt_frame(uint8_t *frame) {
 
     switch (type) {
         case TYPE_PWM:
-            Serial.println(value);
-            pwr = static_cast<int>(value);
+            // Serial.println(value);
+            pwr = value;
+            break;
+        case TYPE_RPM:
+            vt = value;
             break;
         case TYPE_KP:
-            Serial.println(value);
-            // kp = value;
+            // Serial.println(value);
+            kp = value;
             break;
         case TYPE_KI:
-            Serial.println(value);
-            // ki = value;
+            // Serial.println(value);
+            ki = value;
             break;
         case TYPE_KD:
-            Serial.println(value);
-            // kd = value;
+            // Serial.println(value);
+            kd = value;
+            break;
+        case TYPE_START_STOP:
+            if (value == 1.f && !motor_enabled) {
+                // Wznowienie silnika
+                motor_enabled = true;
+
+                if (mode == Mode::Manual) {
+                    pwr = pwr_before_stop;  // Przywróć poprzednie PWM tylko w trybie manualnym
+                }
+            } else if (value == 0.f && motor_enabled) {
+                // Zatrzymanie silnika
+                motor_enabled = false;
+                pwr_before_stop = pwr;  // Zapamiętaj PWM przed zatrzymaniem
+                pwr = 0;
+            }
+            break;
+        case TYPE_MODE:
+            mode = (value < 0.5f) ? Mode::Manual : Mode::Automatic;
             break;
         default:
             Serial.println(type, HEX);
